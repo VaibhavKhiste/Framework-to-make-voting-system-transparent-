190d708f9a4a692f3f05bc197c6bf811eebf3d8a
Vaibhav Khiste <141448092+VaibhavKhiste@users.noreply.github.com>
2025-04-29 21:23:47 +0530

    Add files via upload

---
+++ An_OnlineVotingQR_Admin/config.js
@@ -0,0 +1,8 @@
+gOptions = {
+ 
+  contractaddress: '0x713768EAf5D9BCcEbc785970180C6572D90e947b',
+  adminaddress: '0x83E7689C5C3Cf86B030E79142a0D96a84B2D798c',
+  size: 'big',
+  enabled: true,
+  count: 5
+}
