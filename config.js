c1f2c559f969c9a1d67eee6645ec4aa297e877a1
Vaibhav Khiste <141448092+VaibhavKhiste@users.noreply.github.com>
2025-04-29 21:27:06 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+gOptions = {
+ 
+  contractaddress: '0x713768EAf5D9BCcEbc785970180C6572D90e947b',
+  mailaddress: 'cryptdac@gmail.com',
+  mailpassword: 'qmgvwznbllxdnzwx',
+  size: 'big',
+  enabled: true,
+  count: 5
+}
